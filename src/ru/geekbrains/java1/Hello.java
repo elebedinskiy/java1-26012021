1a74dc59d05dbe9b53e1b70fcf82dcc955eb157f
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-01-26 13:34:19 +0300

    var init

--- src/ru/geekbrains/java1/Hello.java
+++ src/ru/geekbrains/java1/Hello.java
@@ -3,5 +3,17 @@ package ru.geekbrains.java1;
 public class Hello {
     public static void main(String[] args) {
 
+        // Существует 8 примитивных типов данных
+
+        byte varByte = -120; // от -128 до 127
+        short varShort = 12442; // от -32768 до 32767
+        int varInt = 1000; // от -2147483648 до 2147483647
+        long varLong = 200000L; // от -9223372036854775808 до 9223372036854775807
+        float varFloat = 12.23f;
+        double varDouble = -123.123;
+        char varChar1 = '*'; // от '\u0000' или 0 до '\uffff' или 65,535
+        char varChar2 = '\u2242'; // до '\uffff' или 65,535
+        boolean varBoolean = false; // false, true
+
     }
 }
