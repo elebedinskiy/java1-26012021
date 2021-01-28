f6d470298d27521c81e88d36407d64a31b7b89ae
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-01-28 22:46:22 +0300

    task 2

--- src/ru/geekbrains/java1/Hello.java
+++ src/ru/geekbrains/java1/Hello.java
@@ -5,16 +5,23 @@ import java.util.Arrays;
 public class Hello {
     public static void main(String[] args) {
 
-        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        System.out.println(Arrays.toString(arr)); // вывод значений массива в строку
-        for (int i = 0; i < arr.length; i++){
-            if (arr[i] == 0){
-                arr[i] = 1;
+        int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr1)); // вывод значений массива в строку
+        for (int i = 0; i < arr1.length; i++){
+            if (arr1[i] == 0){
+                arr1[i] = 1;
             }
-            else if (arr[i] == 1){
-                arr[i] = 0;
+            else if (arr1[i] == 1){
+                arr1[i] = 0;
             }
         }
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr1));
+
+        int[] arr2 = new int[8];
+        System.out.println(Arrays.toString(arr2));
+        for(int i = 0, j = 0; i < arr2.length; i++, j+=3){
+            arr2[i] = j;
+        }
+        System.out.println(Arrays.toString(arr2));
     }
 }
