628f585788eb06c6e0e34f2a526f606656de1c3c
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-01-28 22:41:37 +0300

    task 1

--- src/ru/geekbrains/java1/Hello.java
+++ src/ru/geekbrains/java1/Hello.java
@@ -1,7 +1,20 @@
 package ru.geekbrains.java1;
 
+import java.util.Arrays;
+
 public class Hello {
     public static void main(String[] args) {
 
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(arr)); // вывод значений массива в строку
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0){
+                arr[i] = 1;
+            }
+            else if (arr[i] == 1){
+                arr[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
