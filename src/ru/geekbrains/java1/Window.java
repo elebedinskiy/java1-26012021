14fc8d7c1118a660954d9b084068fedfaace94aa
Evgeniy Lebedinskiy <el@lecorp.ru>
2021-02-08 23:17:45 +0300

    create window

---
+++ src/ru/geekbrains/java1/Window.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.java1;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    Window(){
+        setSize(600,400);
+        setTitle("Урок 8: практическое задание");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
